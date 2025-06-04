a2cb5c006a6530c4535a129e69c62555481a9f04
allriseyy <allriseyy@gmail.com>
2025-06-04 13:13:31 +0100

    1-first commit

---
+++ keycraft/keycraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  keycraftApp.swift
+//  keycraft
+//
+//  Created by YI YONG LIM on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct keycraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ keycraftTests/keycraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  keycraftTests.swift
+//  keycraftTests
+//
+//  Created by YI YONG LIM on 04/06/2025.
+//
+
+import Testing
+@testable import keycraft
+
+struct keycraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
