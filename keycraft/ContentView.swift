49bb6d7f3c951cf65fc824c2a0ccfd6cc071cf5f
allriseyy <allriseyy@gmail.com>
2025-06-05 00:57:36 +0100

    2 dummy page (#1)
    * 2-dummy page
    
    * 3-gitignore

--- keycraft/ContentView.swift
+++ keycraft/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                Image("charmander")
+                    .resizable()
+                    .cornerRadius(30)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("Charmander")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.orange)
+            }
         }
-        .padding()
     }
 }
 
