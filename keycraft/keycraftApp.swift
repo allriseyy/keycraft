b97d909dcf5e452fd7b1a79e983efaa357aa6596
allriseyy <allriseyy@gmail.com>
2025-06-07 15:52:48 +0100

    4-war card game sample page from https://www.youtube.com/watch?v=2Zi-looUuIA

--- keycraft/keycraftApp.swift
+++ keycraft/keycraftApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct keycraftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            WarCardGameView()
         }
     }
 }
