f67aa93ee713263ffd44815518fa25f784fa07d7
allriseyy <allriseyy@gmail.com>
2025-06-10 19:17:34 +0100

    5-add-functionality and war game

--- keycraft/keycraftApp.swift
+++ keycraft/keycraftApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct keycraftApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
+            ContentView()
             WarCardGameView()
         }
     }
